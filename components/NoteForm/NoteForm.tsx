f55714a87999e595562449e0ef564441880c8a3a
olegnezvuk91 <olegnezvuk@gmail.com>
2025-08-20 02:22:20 +0200

    8hw1

--- components/NoteForm/NoteForm.tsx
+++ components/NoteForm/NoteForm.tsx
@@ -6,14 +6,29 @@ import { useMutation, useQueryClient } from '@tanstack/react-query';
 import type { CreateNote } from '@/types/note';
 import toast from 'react-hot-toast';
 import { useRouter } from 'next/navigation';
+import { useDraftStore } from '@/lib/store/noteStore';
 
 export default function NoteForm() {
   const queryClient = useQueryClient();
   const router = useRouter();
+  const { draft, setDraft, clearDraft } = useDraftStore();
+
+  const handleChange = (
+    event: React.ChangeEvent<
+      HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
+    >,
+  ) => {
+    setDraft({
+      ...draft,
+      [event.target.name]: event.target.value,
+    });
+  };
+
   const { mutate, isPending } = useMutation({
     mutationFn: (newNote: CreateNote) => createNote(newNote),
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['notes'] });
+      clearDraft();
       router.push('/notes/filter/all');
     },
     onError: () => {
@@ -30,7 +45,14 @@ export default function NoteForm() {
     <form className={css.form} action={handleSubmit}>
       <div className={css.formGroup}>
         <label htmlFor="title">Title</label>
-        <input id="title" type="text" name="title" className={css.input} />
+        <input
+          id="title"
+          type="text"
+          name="title"
+          className={css.input}
+          defaultValue={draft?.title}
+          onChange={handleChange}
+        />
       </div>
 
       <div className={css.formGroup}>
@@ -40,12 +62,20 @@ export default function NoteForm() {
           name="content"
           rows={8}
           className={css.textarea}
+          defaultValue={draft?.content}
+          onChange={handleChange}
         />
       </div>
 
       <div className={css.formGroup}>
         <label htmlFor="tag">Tag</label>
-        <select id="tag" name="tag" className={css.select}>
+        <select
+          id="tag"
+          name="tag"
+          className={css.select}
+          defaultValue={draft?.tag}
+          onChange={handleChange}
+        >
           <option value="Todo">Todo</option>
           <option value="Work">Work</option>
           <option value="Personal">Personal</option>
